534cbf85b0b96546b54a9924a721478de31071e2
licheng <lalalic@139.com>
2016-04-05 23:31:34 +0800

    remove lib

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,6 @@
 var gulp=require('gulp'),
     shell=require('gulp-shell');
 
-gulp.task('watch',shell.task('watchify -d -r ./browser.js:docxhub -o dist/docxhub.js'))
-    .task('build', shell.task('babel src --watch --source-maps --out-dir lib'))
-    .task('buildspec', shell.task('babel spec/src --watch --source-maps --out-dir spec/fixture'))
-
-    .task('test', shell.task('jasmine'))
+gulp.task('test', shell.task('jasmine'))
+    .task('test.debug', shell.task('node --debug-brk .jasmine.js'))
     .task('karma', shell.task('karma start'))
-
-    .task('default', function(){
-        gulp.watch(['lib/*.js'],['test'])
-    })
--- src/index.js
+++
@@ -1,2 +0,0 @@
-import Hub from './browser'
-export default Hub
